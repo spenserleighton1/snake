57df165cc1a988dc1f6a793d58e3a48ca481439e
Keeliana Marler <kmmarler2@hotmail.com>
2018-05-02 16:33:57 -0600

    add direction functionality

---
+++ test/index.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+// const Snake = require('../lib/Snake.js');
+
+//
+describe('snake', function() {
+  it('should move up when the user hits the up key', function() {
+    //setup
+    const snake = new Snake;
+    //execution
+    
+    //assertion
+  });
+});
