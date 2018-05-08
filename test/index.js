76c1e4086f51910c0ec9ee309aaa6b844b4c08de
Keeliana Marler <33264293+kmarler2@users.noreply.github.com>
2018-05-07 18:57:07 -0600

    Update index.js
    Add testing information

--- test/index.js
+++ test/index.js
@@ -1,38 +1,46 @@
 const chai = require('chai');
 const expect = chai.expect;
+let Snake = require('../lib/Snake.js');
 
 describe('snake', function() {
-  it('should move when the user hits a coresponding arrow key', function() {
-    //setup
-    //execution
-    //assertion
+  it('should have a tail and a head ', function() {
+    var snake = new Snake(context);
+    snake.move(context);
+    assert.exists(tail);
+    assert.exists(snake.head);
   });
 
-  it('should grow when it eats an apple', function() {
-    //setup
-    //execution
-    //assertion
-  });
-
-  it('should die when it hits a wall', function() {
-    //setup
-    //execution
-    //assertion
-  });
-
-  it('should die when it hits itself', function() {
-    //setup
-    //execution
-    //assertion
+  it('should start with 5 segments', function() {
+    var snake = new Snake();
+    assert.equal(snake.body.length, 5)
   });
 });
 
-describe('food', function() {
-  it('should appear at a random location', function() {
-    //setup
-    //execution
-    //assertion
+describe('game', function() {
+  it('should grow when it eats an apple', function() {
+    var snake = new Snake();
+    assert.equal(game.score, 0);
+    game.eat();
+    assert.equal(game.score, 10);
   });
+
+  it('should die when it hits a wall', function() {
+    var snake = new Snake();
+    game.collisionBorder();
+    assert.equal(game.gameOver, true);
+  });
+
+  it('should die when it hits itself', function() {
+    var snake = new Snake();
+    game.collisionBody();
+    assert.equal(game.gameover, true);
+  });
+
+  it('should not be over initially', function() {
+    var snake = new Snake();
+    assert.equal(game.gameOver, false);
+  });
+});
 
   it('should disappear when eaten', function() {
     //setup
